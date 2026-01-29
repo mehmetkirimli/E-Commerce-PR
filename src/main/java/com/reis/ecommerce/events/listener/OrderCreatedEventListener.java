9a0c540db1b50e9dd916a04ddd97dbf9d5956457
mehmetkirimli <mehmet.kirimli@vbt.com.tr>
2026-01-29 13:02:19 +0300

    fix

--- src/main/java/com/reis/ecommerce/events/listener/OrderCreatedEventListener.java
+++ src/main/java/com/reis/ecommerce/events/listener/OrderCreatedEventListener.java
@@ -2,19 +2,23 @@ package com.reis.ecommerce.events.listener;
 
 import com.reis.ecommerce.events.model.OrderCreatedEvent;
 import com.reis.ecommerce.events.publisher.OrderEventPublisher;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor NOTE: constructor’ı Lombok üretir ama biz burada @Qualifier kullanmak istediğimiz için elle constructor yazıyoruz.
 public class OrderCreatedEventListener
 {
   private final OrderEventPublisher orderEventPublisher;
+
+  public OrderCreatedEventListener(@Qualifier("rabbitMqOrderEventPublisher") OrderEventPublisher orderEventPublisher)
+  {
+    this.orderEventPublisher = orderEventPublisher;
+  }
 
   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
   public void handle(OrderCreatedEvent event)
